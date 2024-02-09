6656eba121d23c4dccb9ee1b17ba919ff308c0c4
dodemabiteniwe <dodemabiteniwe>
2024-02-08 19:59:56 -0500

    changes made from theia lab environment

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const {dispatch,budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
     
@@ -10,12 +10,17 @@ const Budget = () => {
             alert("The budget value cannot exceed 20000 £");
             setNewBudget("20000");
             return;   
-        } else
+        } else{
         setNewBudget(event.target.value);
-    }
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: newBudget,
+        });
+        }
+    };
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £{newBudget}</span>
+<span>Budget: £{budget}</span>
 <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -58,12 +58,25 @@ export const AppReducer = (state, action) => {
                 budget
             };
         case 'SET_BUDGET':
+            let T_exp = 0;
+            T_exp = state.expenses.reduce(
+                (previousExp, currentExp) => {
+                    return previousExp + currentExp.cost
+                },0
+            );
             action.type = "DONE";
             state.budget = action.payload;
-
+            if(T_exp <= state.budget){
             return {
                 ...state,
             };
+        } else {
+            alert("You cannot reduce the budget value lower than the spending");
+            state.budget = T_exp;
+                return {
+                    ...state
+                }
+            }
         case 'CHG_CURRENCY':
             action.type = "DONE";
             state.currency = action.payload;
